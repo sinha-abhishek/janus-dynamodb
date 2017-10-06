b375783b5762cb0e3b33d8663ebbc5d35d7f5983
Abhishek <abhi.bill@gmail.com>
2017-10-06 11:45:16 +0530

    changed some config parameters to work on bigger scale

--- run.sh
+++ run.sh
@@ -17,4 +17,7 @@ echo $env
 echo $zone
 sed -i.bak  "s/\(storage.dynamodb.client.signing-region *= *\).*/\1$zone/g" conf/gremlin-server/dynamodb.properties
 sed -i.bak "s/\(storage.dynamodb.prefix *= *\).*/\1$env/g" conf/gremlin-server/dynamodb.properties
+sed -i.bak "s/\(storage.buffer-size *= *\).*/\14096/g" conf/gremlin-server/dynamodb.properties
+sed -i.bak "s/\(storage.dynamodb.client.executor.max-queue-length *=*\).*\18192/g" conf/gremlin-server/dynamodb.properties
+sed -i.bak "s/\(storage.dynamodb.client.connection-max *= *\).*/\140/g" conf/gremlin-server/dynamodb.properties
 bin/gremlin-server.sh conf/gremlin-server/gremlin-server-edited.yaml
